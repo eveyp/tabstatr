ad12efad2b798e425241fb58ecdaf73f0b791808
Eve Perry <eve@eveperry.com>
2021-09-13 21:05:02 -0700

    create freq function

---
+++ R/freq.R
@@ -0,0 +1,20 @@
+freq = function(data, ..., weights, use_na = c("show", "drop")) {
+  vars = rlang::ensyms(...)
+  if (length(vars) > 2) rlang::abort("More than 2 variables supplied.")
+  if (length(vars) == 0) rlang::abort("No variables supplied.")
+
+  sym_weights = rlang::ensym(weights)
+
+  if (sum(.data[[rlang::as_string(sym_weights)]] < 0) > 0) rlang::abort("Cannot use negative weights.")
+
+  if (missing(use_na) || use_na == "drop") {
+    data = dplyr::filter(data, !is.na(!!vars[[1]]))
+    if (length(vars) == 2) {
+      data = dplyr::filter(data, !is.na(!!vars[[2]]))
+    }
+  }
+
+  data %>%
+    dplyr::group_by(!!!vars) %>%
+    summarise(freq = sum(!!sym_weights, na.rm = TRUE))
+}
