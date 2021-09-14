2e63368f0947211f538f4a6cdca89d70217fea8e
Eve Perry <eve@eveperry.com>
2021-09-13 21:05:54 -0700

    import !!, !!!, %>%

---
+++ R/tabstatr.R
@@ -0,0 +1,12 @@
+#' tabstatr: Summary statistic tables for weighted data
+#'
+#' Creates tables of summary statistics for weighted data. Inspired by the Stata tabstat
+#' command.
+#'
+#' @docType package
+#' @name tabstatr
+#'
+#' @importFrom rlang !!
+#' @importFrom rlang !!!
+#' @importFrom magrittr %>%
+NULL
